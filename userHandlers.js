f1977f715e78ae93e1c3c8d8e168ba420ce33e78
Cylecq <leclercq.cyril06@gmail.com>
2022-12-13 10:40:40 +0100

    Quest done

--- userHandlers.js
+++ userHandlers.js
@@ -1,7 +1,8 @@
 const database = require("./database");
 
 const getUsers = (req, res) => {
-  const initialSql = "select * from users";
+  const initialSql =
+    "select firstname, lastname, email, city, language from users";
   const where = [];
 
   if (req.query.city != null) {
